452a52e54e7c8f603dc9e1216d95ff73131c03c2
Andrew Trent <andrewtrentdesign@gmail.com>
2018-10-19 00:58:15 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |a, b|
     if collection == -1
       collection = a
-    elsif collection > b
+    elsif b < collection
       collection = a
     end
   end
